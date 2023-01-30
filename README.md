32511fed9338632d730738b83a82d75baefe44ae
shuiping <1944680304@qq.com>
2023-01-30 13:58:00 +0800

    update RN_log_filter v1.2 new feature: log_differ

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 此工具用于处理复仇时刻客户端生成client.log，处理后产生的hash结果可用于比较
 
 ## 使用方法
-- 请将client.log放置在软件根目录下
-- 然后运行程序，根目录下会生成一个log_result.txt
+
+- 若首次使用此软件，请先运行一次RN——log_filter.exe，软件会在其根目录自动生成“log_input1”“log_input2”两个文件夹
+- 比对用法
+    - 请将要对比的client.log分别放置在“log_input1”、“log_input2”中
+    - 然后运行程序，软件处理完毕后根目录下会生成一个differ_result.txt，此文件中会包含比对结果
+
+- 只处理单个log
+    - 将client.log放置在“log_input1”、“log_input2”任意一个文件夹即可，然后运行程序，即可在对应的文件夹下看见log_result.txt
+
 
 
 ## 功能
 - [x] 提取log中的hash值并去重以及排序
 - [x] 黑名单功能，黑名单中的内容不会在result中出现
-- [ ] diff功能，读取两份log并自动处理并比较，输出有差异的结果
+- [x] diff功能，读取两份log并自动处理并比较，输出有差异的结果
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 #coding=utf-8
 import os
-import hashlib
 import json
 import logging
 import traceback
-import hashlib
 import operator
 
 log_file_name='RN_log_filter.log'
+
+main_path = '.\\'
+
+log_input_dir_name1 = 'log_input1'
+log_input_dir_name2 = 'log_input2'
+
+result_file_name = "log_result.txt"
+differ_result_name = "differ_result.txt"
+
+Separator_lenth = 100   # 定义分隔符的长度
+Separator = '-' * Separator_lenth  
 
 logformat = "[%(asctime)s] [%(levelname)s] %(msg)s"   #log打印的格式
 
@@ -17,9 +26,11 @@ console_handler.setFormatter(logging.Formatter(logformat))   #设置输出到控
 
 logging.basicConfig(filename=log_file_name,level=logging.DEBUG, format=logformat)
 # 这里定义日志的名字，日志的文本编码，日志的等级
+
 logger = logging.getLogger(__name__)   #logging使用规范，有了这句就可以用logger.xxx了
 
 logger.addHandler(console_handler) #logging输出到控制台
+2
 
 
 
@@ -32,11 +43,36 @@ def clean_old_log():   #把旧的日志清理掉，这样就不会新旧日志�
         else:                #没有log就什么都不做
             return
     except:
-        logging.error(traceback.format_exc())
+        logger.error(traceback.format_exc())
+
+
+def check_make_input_dir(
+        path:str
+):
+    input1_path = os.path.join(path,log_input_dir_name1)
+    input2_path = os.path.join(path,log_input_dir_name2)
+    have_input1 = os.access(input1_path, os.F_OK)
+    have_input2 = os.access(input2_path, os.F_OK)
+    try:
+        if not have_input1:
+            logger.info(f'making dir "log_input1" , path : {input1_path}')
+            os.makedirs(input1_path, exist_ok=True)
+            logger.info(f'making dir "log_input1" successfully')
+        else:
+            logger.info(f'log_input1 dir found')
+        if not have_input2:
+            logger.info(f'making dir "log_input2" , path : {input2_path}')
+            os.makedirs(input2_path, exist_ok=True)
+            logger.info(f'making dir "log_input1" successfully')
+        else:
+            logger.info(f'log_input2 dir found')
+    except:
+        logger.error(traceback.format_exc())
+
         
 
 def have_config():
-    have_config= os.access("RN_log_filter_config.json", os.F_OK)    #判断有没有config.json，返回布尔值
+    have_config= os.access("RN_log_filter_config.json", os.F_OK)    #判断有没有RN_log_filter_config.json，返回布尔值
     if have_config:
         logger.info(f'RN_log_filter_config.json found , reading json')
     if not have_config:
@@ -50,6 +86,7 @@ def read_config_json(
     is_read_config=False
     RN_path = ''
     search_content = []
+    log_differ_error = False
     try:
         path=f'{config_path}RN_log_filter_config.json'
         with open (path,'r',encoding='utf-8') as f:
@@ -58,64 +95,98 @@ def read_config_json(
             search_content = data["search_content"]  # 查找内容
             black_list = data["black_list"] # 这里会读到一个“log文本内容片段”的黑名单列表
             is_read_config = True
-            logger.info(f'read RN_log_filter_config.json successfully , RN_path : "{RN_path}"')
-            logger.info(f'checking search_content list:{search_content}')
-            logger.info(f'checking black_list list:{black_list}')
-            read_clientLog(RN_path,search_content,black_list)
-        return 
+            logger.info(f'read RN_log_filter_config.json successfully')
+            logger.info(f'RN_path : "{RN_path}"')
+            logger.info(f'search_content list:{search_content}')
+            logger.info(f'black_list list:{black_list}')
+            client_log_dir_path1 = os.path.join(RN_path,log_input_dir_name1)
+            client_log_dir_path2 = os.path.join(RN_path,log_input_dir_name2)
+            is_read_client_log1 = read_clientLog(client_log_dir_path1,search_content,black_list)
+            is_read_client_log2 = read_clientLog(client_log_dir_path2,search_content,black_list)
+            if is_read_client_log1 and is_read_client_log2:
+                log_differ_error=log_differ(client_log_dir_path1,client_log_dir_path2,RN_path)
+            if not is_read_client_log1 and not is_read_client_log2:
+                logger.info(f'two client.log not found , skiping log_differ')
+                
+            only_one_log = f'one client.log not found , skiping log_differ'
+            if is_read_client_log1 and not is_read_client_log2:
+                logger.info(only_one_log)
+            if is_read_client_log2 and not is_read_client_log1:
+                logger.info(only_one_log)
+        return is_read_client_log1,is_read_client_log2,log_differ_error
     except:
-        logging.error(f'Error reading config.json,please check log file')
-        logging.error(traceback.format_exc())
+        logger.error(f'Error reading config.json,please check log file')
+        logger.error(traceback.format_exc())
         return 
     
 def write_config_json(
+        path:str
 ): #如果没有config.json，那么这个函数会写一个标准json出来，可惜的是返回的布尔值都没用上
     have_configs = have_config()
     is_write_config=False
     if not have_configs:
         json_dict={}
         try:
-            path = f'.\\RN_log_filter_config.json'
+            path_ = os.path.join(path,'RN_log_filter_config.json')
             RN_path = '.\\'
             black_list = []
             search_content = ['Hash','fh.']
             json_dict['RN_path'] = RN_path
             json_dict['black_list'] = black_list
             json_dict['search_content'] = search_content
-            with open(path,'w',encoding='utf-8') as f:
+            with open(path_,'w',encoding='utf-8') as f:
                 json.dump(json_dict,f,ensure_ascii=False,indent = 4)        
             is_write_config = True
             logger.info(f'writing default RN_log_filter_config.json')        
             return is_write_config,have_configs
 
         except:
-            logging.error(f'Error writing default config.json,please check log file')
-            logging.error(traceback.format_exc())
+            logger.error(f'Error writing default config.json,please check log file')
+            logger.error(traceback.format_exc())
             return is_write_config,have_configs
         
     else:
         return is_write_config,have_configs
     
+def have_client_log(
+        client_log_path:str
+):
+    status = False
+    try:
+        if os.access(client_log_path, os.F_OK):
+            status = True
+        return status
+
+    except:
+        logger.error(traceback.format_exc())
+    
     
     
 def read_clientLog(
-        RN_path:str,
+        log_path:str,
         search_content:list,
         black_list:list
-):      
-    total_hash = 0
-    searching_list = []
-    result_path = os.path.join(RN_path,"log_result.txt")
-    client_log_path = os.path.join(RN_path,"client.log")
+):    
     # i = 0
-    black_list_counter = 0
+    is_read_client_log = False
+    black_list_counter = 0  
     counter1 = 0
     counter2 = 0
+    total_hash = 0
+    searching_list = []
+    result_path = os.path.join(log_path,result_file_name)
+    client_log_path = os.path.join(log_path,"client.log")
+    if have_client_log(client_log_path):
+        logger.info(f'client log found , log path : {client_log_path}')
+    else:
+        logger.info(f'client log not found , please check {client_log_path}')
+        return is_read_client_log
+    
     try:
         with open(client_log_path,'r',encoding='utf-8') as f:
-            logger.info(f'reading client.log')
+            logger.info(f'reading {client_log_path}')
             with open(result_path,'w',encoding='utf-8') as w:
-                logger.info(f'create log_result.txt')
+                logger.info(f'create file : {result_path}')
                 for file_content in f:
                     # print(file_content)
                     for i1 in range(len(search_content)):
@@ -133,65 +204,125 @@ def read_clientLog(
 
                     if not counter1 == 0 and counter2 == 0:
                         # logger.info(file_content)
-                        total_hash += 1
+                        
                         seacrhing_result= file_content[30:]
                         # w.write(seacrhing_result)
                         searching_list.append(seacrhing_result)
+                        # total_hash += 1
                         counter1 = 0 
                         counter2 = 0
                     else:
                         counter1 = 0 
                         counter2 = 0
                 finallist=list(set(searching_list)) 
-                total_hash_ = f'number of hash : {total_hash}'
-                logger.info(total_hash_)
+                # logger.info(total_hash_)
                 finallist.sort(reverse = False) # 逆序排序
                 logger.info(f'sorting result')
                 for ii in finallist:
+                    # logger.info(ii)
+                    total_hash = total_hash + 1
                     w.write(ii)     # 输出结果
-                logger.info(f'writing result to log_result.txt')
+                total_hash_ = f'number of hash : {total_hash}'
+                logger.info(f'writing result to {result_path}')
                 w.write(f' {total_hash_}')  # 总共search到的hash数量
+                is_read_client_log = True
+                return is_read_client_log
                 
 
                 # w.write(total_hash_)
 
     except:
-        logging.error(traceback.format_exc())
+        logger.error(traceback.format_exc())
 
-# def append_list(
-#         file_content:str
-# ):      # 把匹配到的内容全部缝到一个列表里，然后输出这个列表
-#     content_list = []
-#     try:
-#         content_list.append(file_content)   
-#         return content_list
-#     except:
-#         logging.error(traceback.format_exc())
-#         return
-        
+def log_differ(
+        client_log_dir_path1:str,
+        client_log_dir_path2:str,
+        RN_path:str
+): 
+        differ_error = False
+        logger.info(f'running log_differ')
+        differ_list1 = []
+        differ_list2 = []
+        final_list_1 = []
+        final_list_2 = []
+        the_same = []
+        final_list_1_counter = 0
+        final_list_2_counter = 0
+        the_same_counter = 0
+        log_result_path1 = os.path.join(client_log_dir_path1,result_file_name)
+        log_result_path2 = os.path.join(client_log_dir_path2,result_file_name)
+        differ_result_path = os.path.join(RN_path,differ_result_name)
+        try:
+            with open(log_result_path1,'r',encoding='utf-8') as f1:
+                logger.info(f'reading {log_result_path1}')
+                for i1 in f1:
+                    i1.replace('\\','/')
+                    differ_list1.append(i1)
+            with open(log_result_path2,'r',encoding='utf-8') as f2:
+                logger.info(f'reading {log_result_path2}')
+                for i2 in f2:
+                    i2.replace('\\','/')
+                    differ_list2.append(i2)
+            logger.info(f'comparing hash')
+            final_list_1 = list(set(differ_list1).difference(set(differ_list2)))
+            final_list_2 = list(set(differ_list2).difference(set(differ_list1)))
+            the_same = list(set(differ_list1).intersection(set(differ_list2)))
+            logger.info(f'sorting result')
+            final_list_1.sort(reverse = False)
+            final_list_2.sort(reverse = False)
+            the_same.sort(reverse = False)
+            with open(differ_result_path,'w',encoding='utf-8') as f3:
+                logger.info(f'writing differ result--1 in {differ_result_path}')
+                f3.write(f'log_input1 difference hash: \n\n')
+                for i3 in final_list_1:
+                    f3.write(i3)
+                    final_list_1_counter +=1
+                logger.info(f'writing differ result--2 in {differ_result_path}')
+                f3.write(f'\n\n{Separator}\n\n')
+                f3.write(f'log_input2 difference hash: \n\n')
+                for i4 in final_list_2:
+                    f3.write(i4)
+                    final_list_2_counter += 1
+                f3.write(f'\n\n{Separator}\n\n')
+                logger.info(f'writing the same hash  in {differ_result_path}')
+                f3.write(f'the same hash: \n\n')
+                for i5 in the_same:
+                    f3.write(i5)
+                    the_same_counter += 1
+                f3.write(f'\n{Separator}\n\n')
+                logger.info(f'writing hash counter in {differ_result_path}')
+                f3.write(f'number of log_input1 difference hash : {final_list_1_counter}\n')
+                f3.write(f'number of log_input2 difference hash : {final_list_2_counter}\n')
+                f3.write(f'number of same hash : {the_same_counter}\n')
+                return differ_error
+                
+        except:
+            logger.error(f'error of running log_differ , log_differ stop')
+            logger.error(traceback.format_exc())
+            differ_error = True
+            return differ_error
 
-    
-
-
-# def sha256_compute(
-#         file_path:str
-# ): # 得到文件路径后打开文件，计算sha256值，然后返回sha256值
-#     try:
-#         with open (file_path,'rb') as f:
-#             sha256obj = hashlib.sha256()
-#             sha256obj.update(f.read())
-#             hash_value = sha256obj.hexdigest()
-#             return hash_value
-#     except:
-#         logging.error(traceback.format_exc())
 
 
 if __name__ == "__main__":
     # print(list_allfile(r'./'))
+    error_counter = 0
     clean_old_log()
-    write_config_json()
-    read_config_json('.\\')
-    logger.info(f'Done')
+    check_make_input_dir(main_path)
+    write_config_json(main_path)
+    client_log1_status,client_log2_status,differ_error = read_config_json(main_path)
+    if not client_log1_status:
+        error_counter += 1
+    if not client_log2_status:
+        error_counter += 1
+    if differ_error:
+        error_counter += 1
+    if not error_counter == 0:
+        logger.info(f'program exit')
+        exit()
+    else:
+        logger.info(f'Done')
+        # logger.info('123我是中文123')
 
     
     
